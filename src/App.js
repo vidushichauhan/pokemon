4eb183595eb362c271c5553e3ac56fd86786782b
vidushichauhan <vidushic0@gmail.com>
2021-05-09 16:11:21 +0530

    one pokecard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React, { Component } from 'react';
+import Pokecard from './Pokecard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <Pokecard 
+     id={4}
+     name="Charmander"
+     type="fire"
+     base_experience={62}
+     />
     </div>
   );
 }
