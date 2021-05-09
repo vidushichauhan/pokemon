cbf364ce90de4cbd300ea463da74f352459d518a
vidushichauhan <vidushic0@gmail.com>
2021-05-09 16:46:34 +0530

    pokecards aligned

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Pokecard from './Pokecard'
+import './Pokedex.css'
 class Pokedex extends Component{
     static defaultProps={
         pokemon:[
@@ -17,11 +18,13 @@ render(){
 return (
     <div className="Pokedex">
     <h1>Pokedex!</h1>
+    <div className="Pokecard-cards">
     {this.props.pokemon.map((p)=>(
         <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience}/>
     ))
 
     }
+    </div>
     </div>
 )
 
