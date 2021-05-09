bac1fd39b931be50901a6d8fbcc8cccb7034ee43
vidushichauhan <vidushic0@gmail.com>
2021-05-09 18:54:36 +0530

    last commit

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -9,7 +9,9 @@ render(){
     return(
      <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
+        <div className="Pokecard-image">
         <img src={imgSrc} alt={this.props.name}/>
+        </div>
         <div className="Pokecard-data">Type: {this.props.type}</div>
         <div className="Pokecard-data">EXP: {this.props.exp}</div>
     </div>)
