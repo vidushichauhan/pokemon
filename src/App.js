a5a58d098bb78d55add346e0bf948172d7b4b744
vidushichauhan <vidushic0@gmail.com>
2021-05-09 16:38:39 +0530

    pokecards added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import Pokecard from './Pokecard';
+import Pokedex from './Pokedex';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Pokecard 
-     id={4}
-     name="Charmander"
-     type="fire"
-     base_experience={62}
-     />
+     <Pokedex/>
     </div>
   );
 }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Pokecard.css'
 const POKE_API="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
 class Pokecard extends Component{
 render(){
@@ -7,6 +8,8 @@ render(){
      <div className="Pokecard">
         <h1>{this.props.name}</h1>
         <img src={imgSrc} alt={this.props.name}/>
+        <div>Type: {this.props.type}</div>
+        <div>EXP: {this.props.exp}</div>
     </div>)
 }
 }
